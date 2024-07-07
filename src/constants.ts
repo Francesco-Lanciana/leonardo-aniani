e1b3b7b91600283237cc8d1305a4afd7bd2d3331
Francesco Lanciana <francescolanciana@gmail.com>
2024-07-08 05:52:29 +1000

    update username cookie name

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const USER_USERNAME_COOKIE = 'user_info';
+export const USER_USERNAME_COOKIE = 'user_username';
 export const USER_JOBTITLE_COOKIE = 'user_jobtitle';
 export const NEXT_PUBLIC_GRAPHQL_API_URL = 'https://graphql.anilist.co';
