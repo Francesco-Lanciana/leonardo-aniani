28420b6c003ad1ceb1c71e521db69608e5fe0669
Francesco Lanciana <francescolanciana@gmail.com>
2024-07-08 05:52:57 +1000

    complete v1 of profile page
    Allows for changing the username and job title

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,30 +1,64 @@
 'use client';
 
-import styles from './register.module.scss';
+import styles from './profile.module.scss';
 import { useFormState } from 'react-dom';
 
-import { Input } from '@chakra-ui/react';
+import { HStack, Heading, Input, VStack } from '@chakra-ui/react';
 import { Button } from '@chakra-ui/react';
 import { handleUpdateInformation } from '@/actions/user';
+import { useMemo } from 'react';
+import { USER_USERNAME_COOKIE, USER_JOBTITLE_COOKIE } from '@/constants';
+import { useRouter } from 'next/navigation';
+import { getCookie } from '@/lib/cookie-helpers';
 
 export default function Home() {
     const [, formAction, isPending] = useFormState(handleUpdateInformation, null);
+    const router = useRouter();
+    const initalUsername = useMemo(() => getCookie(USER_USERNAME_COOKIE), []);
+    const initialJobTitle = useMemo(() => getCookie(USER_JOBTITLE_COOKIE), []);
+
+    function handleCancel() {
+        router.push('/information');
+    }
 
     return (
         <main className={styles.main}>
-            <div className={styles.description}>
-                <form action={formAction}>
-                    <label htmlFor="username">Username</label>
-                    <Input name="username" required pattern=".*\S.*" />
+            <Heading as="h1" fontSize="4xl" textAlign="center" className={styles.title}>
+                Edit profile
+            </Heading>
 
-                    <label htmlFor="jobtitle">Username</label>
-                    <Input name="jobtitle" required pattern=".*\S.*" />
+            <form action={formAction}>
+                <VStack spacing="1.5rem" className={styles.inputs}>
+                    <VStack spacing="0.5rem" width="100%" alignItems="flex-start">
+                        <label htmlFor="username">Username</label>
+                        <Input name="username" defaultValue={initalUsername} required pattern=".*\S.*" />
+                    </VStack>
+                    <VStack spacing="0.5rem" width="100%" alignItems="flex-start">
+                        <label htmlFor="jobtitle">Job title</label>
+                        <Input name="jobtitle" defaultValue={initialJobTitle} required pattern=".*\S.*" />
+                    </VStack>
+                </VStack>
 
-                    <Button type="submit" disabled={isPending} isLoading={isPending}>
-                        Continue
+                <HStack
+                    spacing="0.5rem"
+                    justifyContent="center"
+                    marginTop="1rem"
+                    className={styles.actionButtons}
+                >
+                    <Button type="button" onClick={handleCancel} variant="outline" color="#F47521">
+                        Cancel
                     </Button>
-                </form>
-            </div>
+                    <Button
+                        type="submit"
+                        disabled={isPending}
+                        isLoading={isPending}
+                        color="#F47521"
+                        variant="solid"
+                    >
+                        Save
+                    </Button>
+                </HStack>
+            </form>
         </main>
     );
 }
