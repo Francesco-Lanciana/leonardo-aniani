7db160e4491771152965499cb7ad2b4c229ac6d9
Francesco Lanciana <francescolanciana@gmail.com>
2024-07-08 05:52:14 +1000

    tidy up layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.scss';
 
-// Without these lines, the icons won't render correctly in server components (the styles are missing)
-// import { config } from '@fortawesome/fontawesome-svg-core';
-// import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS
-// config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
-
 import Providers from './providers';
 
 export const metadata: Metadata = {
-    title: 'AnimeList',
+    title: 'AniAni - Your favorite anime list',
     description: 'A list of all your favorite anime',
 };
 
